58861cb8cedde3b3fc8a40699ad100a8285f179a
Maxime Scharwath <maxscharwath@gmail.com>
2026-08-24 12:05:31 +0200

    test(torrents): expect indexer dep ^0.4.0 to match bumped manifest

--- modules/tv.kroma.torrents/ui/src/index.test.ts
+++ modules/tv.kroma.torrents/ui/src/index.test.ts
@@ -5,7 +5,7 @@ import { torrentsModule } from './index';
 describe('torrentsModule', () => {
   it('takes its identity and its dependencies from the shared manifest', () => {
     expect(torrentsModule.id).toBe('tv.kroma.torrents');
-    expect(torrentsModule.dependencies).toEqual({ 'tv.kroma.indexer': '^0.3.0' });
+    expect(torrentsModule.dependencies).toEqual({ 'tv.kroma.indexer': '^0.4.0' });
     expect(torrentsModule.optionalDependencies).toEqual({ 'tv.kroma.vpn': '^0.3.0' });
   });
 
